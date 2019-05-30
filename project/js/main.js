3350e6db2f7a35f3d06d327752fddcdb0a474c5c
dkosinov <dkosinov@mail.ru>
2019-05-30 16:03:24 +0300

    Оптимизация кода

--- project/js/main.js
+++ project/js/main.js
@@ -5,17 +5,12 @@ const products = [
     {id: 4, title: 'Gamepad', price: 75},
 ];
 
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="buy-btn">Купить</button>
-            </div>`
-};
-
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
-    document.querySelector('.products').innerHTML = productsList;
+    document.querySelector('.products').innerHTML = list.map(item => `<div class="product-item">
+                <h3>${item.title}</h3>
+                <p>${item.price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`);
 };
 
 renderPage(products);
